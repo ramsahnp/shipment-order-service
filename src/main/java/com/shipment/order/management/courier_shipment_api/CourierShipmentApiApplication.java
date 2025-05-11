aad979d22f273fa93054b4f52b7e0951670b3a1c
ramsahnp <ramsahnp@gmail.com>
2025-05-11 18:22:05 +0530

    first commit

---
+++ src/main/java/com/shipment/order/management/courier_shipment_api/CourierShipmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.shipment.order.management.courier_shipment_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierShipmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierShipmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shipment/order/management/courier_shipment_api/CourierShipmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipment.order.management.courier_shipment_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierShipmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
